9efe8c00e3ae5ab0b923675e15f3b2d928f6fb88
dandamdandam <glue0440@gmail.com>
2024-09-19 14:55:03 +0900

    init/라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([]);
+import Layout from './components/Layout';
+import ColumnDetail from './pages/ColumnDetail';
+import ColumnList from './pages/ColumnList';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import MyAccount from './pages/MyAccount';
+import NotFound from './pages/NotFound';
+import ProductDetail from './pages/ProductDetail';
+import ProductList from './pages/ProductList';
+import { RouterPath } from './shared/path';
+
+const router = createBrowserRouter([
+  {
+    path: RouterPath.root,
+    element: <Layout />,
+    children: [
+      {
+        path: RouterPath.home,
+        element: <Home />,
+      },
+      {
+        path: RouterPath.productList,
+        element: <ProductList />,
+      },
+      {
+        path: RouterPath.columnList,
+        element: <ColumnList />,
+      },
+      {
+        path: RouterPath.columnDetail,
+        element: <ColumnDetail />,
+      },
+      {
+        path: RouterPath.productDetail,
+        element: <ProductDetail />,
+      },
+      {
+        path: RouterPath.myAccount,
+        element: <MyAccount />,
+      },
+      {
+        path: RouterPath.login,
+        element: <Login />,
+      },
+      {
+        path: RouterPath.notFound,
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
