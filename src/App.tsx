265ea531fc628df0c7ac80c2e783e9f6bd3ac7cf
dandamdandam <glue0440@gmail.com>
2024-09-23 19:19:57 +0900

    fix: 절대경로 관련

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { css, Global } from '@emotion/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Layout from './components/Layout';
-import ColumnDetail from './pages/ColumnDetail';
-import ColumnList from './pages/ColumnList';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import MyAccount from './pages/MyAccount';
-import NotFound from './pages/NotFound';
-import ProductDetail from './pages/ProductDetail';
-import ProductList from './pages/ProductList';
-import { RouterPath } from './shared/path';
+import Layout from '@/components/Layout';
+import ColumnDetail from '@/pages/ColumnDetail';
+import ColumnList from '@/pages/ColumnList';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import MyAccount from '@/pages/MyAccount';
+import NotFound from '@/pages/NotFound';
+import ProductDetail from '@/pages/ProductDetail';
+import ProductList from '@/pages/ProductList';
+import { RouterPath } from '@/utils/path';
 
 const router = createBrowserRouter([
   {
