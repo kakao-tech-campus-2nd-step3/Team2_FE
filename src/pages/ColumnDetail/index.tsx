a364915bc120398006244a7dbd2b82d0fcf7f420
dandamdandam <glue0440@gmail.com>
2024-09-26 21:56:56 +0900

    feat: column header

--- src/pages/ColumnDetail/index.tsx
+++ src/pages/ColumnDetail/index.tsx
@@ -1,3 +1,37 @@
+import Header from './Header';
+
+const data = {
+  id: '1',
+  title: '칼럼의 제목이 여기에 들어갑니다',
+  imgurl: 'https://cdn.imweb.me/upload/S2017101359e025984d346/ad539f598e444.jpg',
+  createdAt: '2020-01-01',
+  auth: '작성자',
+  keyword: ['키워드1', '키워드2', '키워드3'],
+  content: [
+    {
+      tag: 'h2',
+      content: '제목입니다.',
+    },
+    {
+      tag: 'p',
+      content: '내용입니다.',
+    },
+    {
+      tag: 'img',
+      content: 'https://cdn.imweb.me/upload/S2017101359e025984d346/ad539f598e444.jpg',
+    },
+  ],
+};
 export default function ColumnDetail() {
-  return <div>ColumnDetail</div>;
+  return (
+    <>
+      <Header
+        title={data.title}
+        imgurl={data.imgurl}
+        createdAt={data.createdAt}
+        auth={data.auth}
+        keyword={data.keyword}
+      />
+    </>
+  );
 }
