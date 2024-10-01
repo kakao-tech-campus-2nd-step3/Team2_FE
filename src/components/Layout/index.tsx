de3b4ff86182187b72d5ab707afbcffd26c6c376
codus1718 <chaeyeon1718@daum.net>
2024-10-01 14:18:38 +0900

    feat: Footer 구현

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
@@ -7,6 +8,7 @@ export default function Layout() {
       Layout
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
