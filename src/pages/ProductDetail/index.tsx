bb60b3bfe2efe0b9b757fc405c8b5bab9cb842cf
95smile_ <57226469+codus1718@users.noreply.github.com>
2024-10-12 17:11:19 +0900

    feat: 상품 상세 페이지 - ProductInfo 구현

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,3 +1,29 @@
-export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+import { JSX } from "react";
+
+import ProductInfo from "./ProductInfo";
+import type { ProductDetail } from "./type";
+
+const data: ProductDetail = {
+  id: "1",
+  name: "상품명",
+  price: "5000",
+  imgurl: "https://cdn.imweb.me/upload/S2017101359e025984d346/ad539f598e444.jpg",
+  totalrate: "3.8",
+  moreinfo: "추가 설명",
+  producturl:
+    "https://www.getyourguide.com/ko-kr/roma-l33/roma-kolroseum-igseupeureseu-tueo-mic-poreom-ibjang-t457508/?ranking_uuid=db08e83a-62a1-4717-9945-6a2da1739289",
+  imageurl2: "https://cdn.imweb.me/upload/S2017101359e025984d346/ad539f598e444.jpg",
+  freformcate: ["글로텐 프리"],
+  allregycate: ["땅콩", "카테고리2"],
+};
+
+/**
+ * @returns {JSX.Element} - 상품 상세 페이지를 렌더링하는 JSX 요소
+ */
+export default function ProductDetail(): JSX.Element {
+  return (
+    <>
+      <ProductInfo product={data} />
+    </>
+  );
 }
