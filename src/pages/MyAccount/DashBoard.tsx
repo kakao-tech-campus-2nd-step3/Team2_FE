13bf31ca4a9a24e4b7dd4b2d64e434e874bb47bd
dandamdandam <glue0440@gmail.com>
2024-10-28 14:41:46 +0900

    feat: mypage 화면1

---
+++ src/pages/MyAccount/DashBoard.tsx
@@ -0,0 +1,76 @@
+import styled from "@emotion/styled";
+
+import Categories from "@/components/Categories";
+import Spacing from "@/components/Spacing";
+
+type Props = {
+  allergies: string[];
+  freefrom: string[];
+};
+
+export default function DashBoard({ allergies, freefrom }: Props) {
+  return (
+    <Container>
+      <div>
+        <H3>내가 선택한 알러지 항목</H3>
+        <Categories categories={allergies} />
+        <Spacing size={30} />
+        <H3>내가 선택한 프리프롬 항목</H3>
+        <Categories categories={freefrom} />
+      </div>
+      <div>
+        <H3>기록</H3>
+        <BtnWithIcon>
+          <span className="material-symbols-outlined">edit_square</span>나의 리뷰
+        </BtnWithIcon>
+        <BtnWithIcon>
+          <span className="material-symbols-outlined">favorite</span>찜 목록
+        </BtnWithIcon>
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div({
+  height: "45vh",
+  display: "grid",
+  gridTemplateColumns: "4.5fr 1fr",
+  margin: "1rem",
+  padding: "1rem",
+
+  border: `var(--color-gray) 1px solid`,
+  borderRadius: "0.5rem",
+  "@media (max-width: 768px)": {
+    gridTemplateColumns: "1fr",
+    "& > div:nth-of-type(1)::after": {
+      content: '""',
+      display: "block",
+      width: "100%",
+      height: "1px",
+      marginTop: "1.5rem",
+      backgroundColor: "var(--color-gray)",
+    },
+  },
+});
+
+const H3 = styled.h3({
+  fontSize: "var(--font-size-large)",
+  fontWeight: "bold",
+  padding: "0.7rem 0 1rem",
+});
+
+const BtnWithIcon = styled.button({
+  border: `var(--color-gray) 1px solid`,
+  borderRadius: "0.5rem",
+  padding: "0.2rem",
+  marginBottom: "0.5rem",
+  width: "100%",
+
+  display: "flex",
+  alignItems: "center",
+  gap: "0.3rem",
+  fontSize: "var(--font-size-base)",
+  ".material-symbols-outlined": {
+    fontVariationSettings: "'wght' 200",
+  },
+});
--- src/pages/MyAccount/index.tsx
+++ src/pages/MyAccount/index.tsx
@@ -1,3 +1,40 @@
+import styled from "@emotion/styled";
+
+import DashBoard from "./DashBoard";
+
+// /api/users/info
+const userInfo = {
+  id: 1,
+  user_name: "이름",
+  user_img_url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+  allergies: ["달걀", "이름이 길이이일다란 알러지"],
+  freefrom: ["글루텐프리"],
+};
+
 export default function MyAccount() {
-  return <div>MyAccount</div>;
+  return (
+    <>
+      <Profile>
+        <img src={userInfo.user_img_url} alt="user profile image" />
+        <h2>{userInfo.user_name}</h2>
+      </Profile>
+      <DashBoard allergies={userInfo.allergies} freefrom={userInfo.freefrom} />
+    </>
+  );
 }
+
+const Profile = styled.div`
+  margin: 2rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  img {
+    width: 8rem;
+    height: 8rem;
+    border-radius: 50%;
+  }
+  h2 {
+    font-size: 1.2rem;
+  }
+`;
