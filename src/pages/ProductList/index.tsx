363622f62b3cd3dfbafe8a174fc6fb08eed14628
95smile_ <57226469+codus1718@users.noreply.github.com>
2024-11-05 22:56:02 +0900

    feat: productList 초안

--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -1,3 +1,66 @@
+import styled from "@emotion/styled";
+
+import Pagination from "@/components/Pagination";
+import SortingBtns from "@/components/SortingBtns";
+
 export default function ProductList() {
-  return <div>ProductList</div>;
+  return (
+    <>
+      <SortingBtnsSection>
+        <SortingBtns
+          sortingBtns={[
+            {
+              name: "신상품",
+              value: "recent",
+            },
+            {
+              name: "가격 순",
+              value: "price",
+            },
+            {
+              name: "추천 순",
+              value: "recommendations",
+            },
+            {
+              name: "구매량",
+              value: "Purchases",
+            },
+          ]}
+        />
+      </SortingBtnsSection>
+
+      <ProductListSection></ProductListSection>
+
+      <Pagination
+        // totalResults={data.pageInfo.totalResults}
+        // resultsPerPage={data.pageInfo.resultsPerPage}
+        totalResults={3}
+        resultsPerPage={10}
+      />
+    </>
+  );
 }
+
+const SortingBtnsSection = styled.section({
+  display: "flex",
+  justifyContent: "right",
+  width: "65%",
+  margin: "0 auto",
+  padding: "1rem 0",
+  "@media (max-width: 768px)": {
+    width: "90%",
+  },
+});
+
+const ProductListSection = styled.section({
+  width: "65%",
+  margin: "0 auto",
+
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gap: "1rem",
+  "@media (max-width: 768px)": {
+    width: "90%",
+    gridTemplateColumns: "1fr",
+  },
+});
