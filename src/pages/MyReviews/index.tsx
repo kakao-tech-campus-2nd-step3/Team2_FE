ad96deb1213d1efc1967957433dbec63ce13f4e9
dandamdandam <glue0440@gmail.com>
2024-11-08 22:06:40 +0900

    feat: my reviews 가져옴

--- src/pages/MyReviews/index.tsx
+++ src/pages/MyReviews/index.tsx
@@ -1,8 +1,23 @@
 import styled from "@emotion/styled";
+import { useQuery } from "@tanstack/react-query";
 
 import Background from "@/components/Background";
+import { fetchInstance } from "@/utils/axiosInstance";
 
 export default function MyReviews() {
+  const { data, isPending, isError } = useQuery<object>({
+    queryKey: ["myReviews"],
+    queryFn: async () => {
+      const response = await fetchInstance().get("/api/reviews/my");
+      console.log(response.data);
+      return response.data;
+    },
+  });
+
+  if (isPending) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  console.log(data); // TODO 리뷰 컴포넌트로 랜더링
   return (
     <Background>
       <H1>나의 리뷰</H1>
